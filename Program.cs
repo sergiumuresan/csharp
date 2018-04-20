141cb04ba9455486573b719a59a98dcef1a26423
Sergiu Muresan <smure@nets.eu>
2018-04-20 16:20:29 +0300

    add all examples

--- Program.cs
+++ Program.cs
@@ -8,8 +8,24 @@ namespace csharp
         static void Main(string[] args)
         {
             Console.WriteLine("PrimeNumbers");
-
+           
             PrimeNumbers.DoWork();
+
+            Console.WriteLine("\nPrimeNumbersTask");
+
+            PrimeNumbersTask.DoWork();
+
+            Console.WriteLine("\nPrimeNumbersAsync");
+
+            PrimeNumbersAsync.DoWork();
+
+            Console.WriteLine("\nPrimeNumbersParallel");
+
+            PrimeNumbersParallel.DoWork();
+
+            Console.WriteLine("\nPrimeNumbersThreadPool");
+
+            PrimeNumbersThreadPool.DoWork();
 
             Console.ReadKey();
         }
