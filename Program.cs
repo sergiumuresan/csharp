1a0086150e247a5c24ee510416606d38a42726ec
Sergiu Muresan <smure@nets.eu>
2018-05-23 14:39:43 +0300

    add session 5

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using csharp.Session1;
+﻿using csharp.Session3;
+using csharp.Session5;
 using System;
  
 namespace csharp
@@ -7,27 +8,8 @@ namespace csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("PrimeNumbers");
-           
-            PrimeNumbers.DoWork();
-
-            Console.WriteLine("\nPrimeNumbersTask");
-
-            PrimeNumbersTask.DoWork();
-
-            Console.WriteLine("\nPrimeNumbersAsync");
-
-            PrimeNumbersAsync.DoWork();
-
-            Console.WriteLine("\nPrimeNumbersParallel");
-
-            PrimeNumbersParallel.DoWork();
-
-            Console.WriteLine("\nPrimeNumbersThreadPool");
-
-            PrimeNumbersThreadPool.DoWork();
-
-            Console.ReadKey();
+            AESExample.EncryptSomeText();
+            RSAExample.EncryptSomeText();
         }
     }
 }
---
+++ Session3/Deck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp.Session3
+{
+    public class Deck<T> where T : class
+    {
+        private IList<T> Items { get; set; }
+
+        public T this[int index]
+        {
+            get { return Items[index]; }
+        }
+
+        public void Add(T item) {
+            this.Items.Add(item);
+        }
+        
+        public Deck()
+        {
+            this.Items = new List<T>();
+        }
+    }
+}
